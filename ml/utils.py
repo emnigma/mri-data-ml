87bc0bf16297baf8f0d50743023c7eacaa6b9750
Max Nigmatulin <mvnigma@gmail.com>
2024-02-16 17:27:35 +0300

    Upd ml/utils, add catboost

--- ml/utils.py
+++ ml/utils.py
@@ -26,27 +26,45 @@ def load_dataset() -> tuple[pd.DataFrame, pd.Series]:
 def load_cleaned_dataset() -> tuple[pd.DataFrame, pd.Series]:
     X, y = load_dataset()
 
+    X = X[
+        [
+            "wm_snr_norm",
+            "holes_lh",
+            "holes_rh",
+            "defects_lh",
+            "defects_rh",
+            "con_snr_lh",
+            "con_snr_rh",
+        ]
+    ]
+
+    X["defects"] = X["defects_lh"] + X["defects_rh"]
+    X["con_snr"] = X["con_snr_lh"] * X["con_snr_rh"]
+    X["holes"] = X["holes_lh"] + X["holes_rh"]
+
+    X = X.drop(["defects_lh", "defects_rh", "holes_lh", "holes_rh"], axis=1)
+
+    return X, y
+
+
+def load_picked_dataset() -> tuple[pd.DataFrame, pd.Series]:
+    X, y = load_dataset()
+
     X = X.drop(
         [
-            "wm_snr_orig",
-            "gm_snr_orig",
-            "cc_size",
-            "topo_lh",
-            "topo_rh",
             "rot_tal_x",
             "rot_tal_y",
             "rot_tal_z",
+            "cc_size",
         ],
         axis=1,
     )
 
-    X["Defects"] = X["defects_lh"] + X["defects_rh"]
-    X["CON_SNR"] = X["con_snr_lh"] + X["con_snr_rh"]
+    # X["Defects"] = X["defects_lh"] + X["defects_rh"]
+    # X["CON_SNR"] = X["con_snr_lh"] + X["con_snr_rh"]
 
-    X = X.drop(
-        ["gm_snr_norm", "holes_lh", "holes_rh", "con_snr_lh", "con_snr_rh"], axis=1
-    )
+    # X = X.drop(["gm_snr_norm", "holes_lh", "holes_rh"], axis=1)
 
-    X = X[["Defects", "CON_SNR", "wm_snr_norm"]]
+    # X = X[["Defects", "con_snr_lh", "con_snr_rh", "wm_snr_norm"]]
 
     return X, y
